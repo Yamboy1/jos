14885840741c6d5dd4a64605205f47c05ef3b727
jquinn <>
2000-04-07 02:24:09 +0000

    Testing SourceForge CVS import. -_Quinn

---
+++ test/test.java
@@ -0,0 +1,10 @@
+import java.io.*;
+
+public class test {
+    public static void main( String [] args ) {
+        System.out.println( "arguments:" );
+        for ( int x = 0; x < args.length; x ++ ) {
+            System.out.println( args[x] );
+            }
+        }
+    }
