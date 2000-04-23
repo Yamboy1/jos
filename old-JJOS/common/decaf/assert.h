d6239bbdda15e1886c66ab7f8b187fecf8c8bf9c
jquinn <>
2000-04-23 05:02:16 +0000

    Commented ASSERT_CAST in assert.h; reverted the nativecode Makefile.

--- old-JJOS/common/decaf/assert.h
+++ old-JJOS/common/decaf/assert.h
@@ -16,11 +16,16 @@
 #define ASSERT_NOT_NULL(ptr,loc,error) \
 	if(ptr==NULL) {kprintf("%s -- %s, aborting.\n",loc,error); abort();}
 
-/* if src is not of type dst, something terrible has happened to the JVM. */
 #ifndef DONT_USE_DC
+/* is 'dst' of type 'type'?  if so, cast it into dst; otherwise,
+   loc is where in the code we are, and thing is what the cast
+   is checking for. */
 #define ASSERT_CAST(dst,src,type,loc,thing) \
 	if((dst=dynamic_cast<type>(src))==0) { kprintf("%s -- %x is not a %s, aborting.\n",loc,src,thing); abort(); }
 #else
+/* type1 is a type() return, and type2 the constant against
+   which to check it.  pos is where in the code we are, thing is
+   what the cast is checking for, and dst the pointer. */
 #define ASSERT_CAST(dst,type1,type2,pos,thing) \
 	if(type1!=type2) { kprintf("%s -- %x is not a %s, aborting.\n", pos, dst, thing ); abort(); }
 
