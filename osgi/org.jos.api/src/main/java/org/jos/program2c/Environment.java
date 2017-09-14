915572db085af000d02eb2d57a6c6bc47fb6ebcf
gchii <>
2017-09-14 10:07:03 +0000

    Updates Environment interface

--- osgi/org.jos.api/src/main/java/org/jos/program2c/Environment.java
+++ osgi/org.jos.api/src/main/java/org/jos/program2c/Environment.java
@@ -2,9 +2,16 @@ package org.jos.program2c;
 
 public interface Environment {
 	/**
-	 * Gets Attribute property.
+	 * Gets Attribute[] property.
 	 * @param key
 	 * @return
 	 */
 	public abstract String getAttribute(String key);
+
+	/**
+	 * Sets Attribute[] property.
+	 * @param key
+	 * @param value
+	 */
+	public void setAttribute(String key, Object value);
 }
